1489a94539e70cd9708ab376e77e56e66b4a578b
buko106 <buko1062000@yahoo.co.jp>
2017-04-30 01:34:48 +0900

    50*50 image can be obtained

--- searchFace.py
+++ searchFace.py
@@ -16,6 +16,10 @@ def checkImage( img , winName="No Name" , pos=(0,0) , wait=0 ):
     # Destroy
     cv2.destroyAllWindows()
 
+def cutImege( img , pos ):
+    x , y , w , h = pos
+    return img[y:y+h,x:x+w]
+
 def detectEyes(face,cascade_eye,size=None,isBGR=False):
     
     r = face.shape[0]
@@ -63,7 +67,7 @@ def detectFaces(img,cascade_face,size=None,isBGR=False):
     return ret
 
 
-def detectFaceWithRotation(img,cascade_f,cascade_e,size=None,faceSize=None,isBGR=False):
+def detectFaceWithRotation(img,cascade_f,cascade_e,size=None,faceSize=None,isBGR=False,deglist=range(-20,21,5)):
     
     r = img.shape[0]
     c = img.shape[1]
@@ -89,27 +93,68 @@ def detectFaceWithRotation(img,cascade_f,cascade_e,size=None,faceSize=None,isBGR
     frame = numpy.zeros((hypot, hypot), numpy.uint8)
     frame[ int((hypot - rows) * 0.5): int((hypot + rows) * 0.5) , int((hypot - cols) * 0.5) : int((hypot + cols) * 0.5) ] = gray
 
-    for deg in range(-28, 29, 7):
+
+    facesWithRank = []
+    for deg in deglist:
         M = cv2.getRotationMatrix2D((hypot * 0.5, hypot * 0.5), deg, 1.0)
         rotated = cv2.warpAffine(frame, M, (hypot, hypot))
         # detect face
+        
         faces = detectFaces(rotated,cascade_f)
+        faces = [ (x,y,w,h) for (x,y,w,h) in faces if w*5 > cols and h*5 > rows ] # small face is eliminated
+        if (not faces): # face not found
+            continue
+        """
+        temp = numpy.copy(rotated)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(temp, (x, y) , (x + w, y + h), (150, 150, 150), 2)
+
+        checkImage(temp,wait=300)
+        """
+        
 
         for (x, y, w, h) in faces:
             face = rotated[y:y+h,x:x+w]
             eyes = detectEyes(face,cascade_e,faceSize)
-            for ( x , y , w , h ) in eyes:
-                cv2.rectangle(face, (x, y), (x + w, y + h), (0, 0, 0), 2)
-            if eyes :
-                checkImage(face,"Detected eyes",wait=100)
-        # if faces == () : continue
-        # checkImage(rotated,file+"deg="+str(deg)+")", wait=300)
+
+            # eyes are in upper half of  face
+            upper = [ (ex,ey,ew,eh) for (ex,ey,ew,eh) in eyes if ey+eh/2 <= y+h/2 and eh*10 >= h ]
+            
+            # for ( x , y , w , h ) in upper:
+            #    cv2.rectangle(face, (x, y), (x + w, y + h), (0, 0, 0), 2)
+            # print upper
+            # checkImage(face,wait=200)
+
+            if len(upper) == 0 :
+                facesWithRank += [ ( 1.0 , numpy.abs(deg) , (x,y,w,h) , deg) ]
+            elif len(upper) == 1 or len(upper) > 2 :
+                facesWithRank += [ ( 0.5 , numpy.abs(deg) , (x,y,w,h) , deg) ]
+            else :
+                _ , y0 , _ , h0 = upper[0]
+                _ , y1 , _ , h1 = upper[1]
+                y0 = float(y0);y1 = float(y1);h0 = float(h0);h1 = float(h1)
+                facesWithRank += [ (numpy.abs((y0+h0/2)-(y1+h1/2))/h , numpy.abs(deg) ,(x,y,w,h), deg )]
+        
+    facesWithRank.sort()
+    # print facesWithRank
+    if not facesWithRank : return None
+    best = facesWithRank[0]
+    _ , _ , (x,y,w,h) , deg = best
+    return ( deg , (int(x/ratio),int(y/ratio),int(w/ratio),int(h/ratio)) )
+    M = cv2.getRotationMatrix2D((hypot * 0.5, hypot * 0.5), deg, 1.0)
+    rotated = cv2.warpAffine(frame, M, (hypot, hypot))
+    # detect face
+    checkImage( cutImege( rotated , best[2] ) , wait=500 )
+    #if upper : checkImage(face,"Detected eyes",wait=300)
+    # if faces == () : continue
+    # checkImage(rotated,file+"deg="+str(deg)+")", wait=300)
 
 
 def findAllImage(src):
+    src = src.rstrip("/")
     for root , dir , files in os.walk(src):
         for file in files:
-            yield os.path.join(root,file)
+            yield os.path.join(root,file)[len(src)+1:]
 
 
 desc="Detect faces from all image files under SRC"
@@ -126,11 +171,35 @@ eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye.xml')
 
 
 # Image Files Path
-sourcedir = args.src
+source = args.src
+destination = args.dst
 # Get a list of all Images
-for file in findAllImage(sourcedir):
-    print file
+for file in findAllImage(source):
+    print "Read from : "+os.path.join(source,file)
     # イメージファイルの読み込み
-    img = cv2.imread(file)
+    img = cv2.imread(os.path.join(source,file))
     if None == img : continue
-    detectFaceWithRotation(cv2.imread(file),face_cascade,eye_cascade,size=200,faceSize=200,isBGR=True)
+    result = detectFaceWithRotation(img,face_cascade,eye_cascade,size=300,faceSize=200,isBGR=True,deglist=range(-35,36,5))
+    if not result : continue
+    else : deg , pos = result
+
+    rows, cols , _ = img.shape
+    hypot = int(math.hypot(rows, cols))
+    M = cv2.getRotationMatrix2D((hypot * 0.5, hypot * 0.5), deg, 1.0)
+    # cv2 image is type=numpy.ndarray of numpy.uint8
+    frame = numpy.zeros((hypot, hypot,3), numpy.uint8)
+    frame[ int((hypot - rows) * 0.5): int((hypot + rows) * 0.5) , int((hypot - cols) * 0.5) : int((hypot + cols) * 0.5) ] = img
+    rotated = cv2.warpAffine(frame, M, (hypot, hypot))
+    # x,y,w,h = pos ; cv2.rectangle( rotated , (x,y) , (x+w,y+h) , (numpy.random.randint(0,256),numpy.random.randint(0,256),numpy.random.randint(0,256)) , 2 )
+    
+    root, ext = os.path.splitext(os.path.join(destination,file) )
+    imgPath = root + ".jpeg"
+    path , name = os.path.split(imgPath)
+    try:
+        os.makedirs(path)
+    except OSError :
+        pass
+
+    print "Write to : "+imgPath
+    cv2.imwrite(imgPath,cv2.resize(cutImege(rotated,pos),(50,50)))
+    
